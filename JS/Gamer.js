ce5a158076823c54adaba41db528c0b5d369b529
DANKELDIGITAL <kwaivideos930@gmail.com>
2024-11-07 09:48:53 -0500

    DANKEL DIGITAL
    DANKEL DIGITAL TIENDA

---
+++ JS/Gamer.js
@@ -0,0 +1,28 @@
+document.getElementById('comentariosForm').addEventListener('submit', function(event) {
+    event.preventDefault();
+
+
+    const nombre = document.getElementById('nombre').value;
+    const correo = document.getElementById('correo').value;
+    const comentario = document.getElementById('comentario').value;
+
+
+    const nuevoComentario = {
+        nombre: nombre,
+        correo: correo,
+        comentario: comentario,
+        fecha: new Date().toLocaleString()
+    };
+
+
+    let comentarios = JSON.parse(localStorage.getItem('comentarios')) || [];
+    comentarios.push(nuevoComentario);
+
+
+    localStorage.setItem('comentarios', JSON.stringify(comentarios));
+
+
+    document.getElementById('comentariosForm').reset();
+
+    alert('¡Gracias por tu comentario! Se ha guardado correctamente.');
+});
